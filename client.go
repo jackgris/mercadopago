71e06195e388568fe4cd1a0992af228fc2c5f3cc
Gabriel Pozo <jackgris2@gmail.com>
2023-08-05 22:02:11 -0300

    add: first API interaction we get the methods payment available
    Signed-off-by: Gabriel Pozo <jackgris2@gmail.com>

--- client.go
+++ client.go
@@ -1,11 +1,38 @@
 package mercadopago
 
-type client struct {
-	token string
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+const (
+	baseURLV1 = "https://api.mercadopago.com/v1/"
+)
+
+// Client is the API client
+type Client struct {
+	token      string
+	BaseURL    string
+	HTTPClient *http.Client
 }
 
-func NewClient(token string) *client {
-	return &client{
-		token: token,
+// NewClient create a new Client for API interaction
+func NewClient(token string) *Client {
+	return &Client{
+		BaseURL: baseURLV1,
+		token:   token,
+		HTTPClient: &http.Client{
+			Timeout: time.Minute,
+		},
 	}
+}
+
+// PrettyStruct print out JSON response in a pretty way
+func PrettyStruct(data interface{}) (string, error) {
+	val, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return "", err
+	}
+	return string(val), nil
 }
---
+++ payment_methods.go
@@ -0,0 +1,97 @@
+package mercadopago
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type PaymentMethods []PaymentMethod
+
+type PaymentMethod struct {
+	ID                    string     `json:"id"`
+	Name                  string     `json:"name"`
+	PaymentTypeID         string     `json:"payment_type_id"`
+	Status                string     `json:"status"`
+	SecureThumbnail       string     `json:"secure_thumbnail"`
+	Thumbnail             string     `json:"thumbnail"`
+	DeferredCapture       string     `json:"deferred_capture"`
+	Settings              Settings   `json:"settings"`
+	AdditionalInfoNeeded  []struct{} `json:"additional_info_needed"`
+	MinAllowedAmount      float64    `json:"min_allowed_amount"`
+	MaxAllowedAmount      int        `json:"max_allowed_amount"`
+	AccreditationTime     int        `json:"accreditation_time"`
+	FinancialInstitutions struct{}   `json:"financial_institutions"`
+	ProcessingModes       string     `json:"processing_modes"`
+}
+
+type Settings struct {
+	Bin          Bin          `json:"bin"`
+	CardNumber   CardNumber   `json:"card_number"`
+	SecurityCode SecurityCode `json:"security_code"`
+}
+
+type Bin struct {
+	Pattern             string `json:"pattern"`
+	ExclusionPattern    string `json:"exclusion_pattern"`
+	InstallmentsPattern string `json:"installments_pattern"`
+}
+
+type CardNumber struct {
+	Length     int    `json:"length"`
+	Validation string `json:"validation"`
+}
+
+type SecurityCode struct {
+	Mode         string `json:"mode"`
+	Length       int    `json:"length"`
+	CardLocation string `json:"card_location"`
+}
+
+type ErrorResponse struct {
+	Message string        `json:"message"`
+	Error   string        `json:"error"`
+	Status  int           `json:"status"`
+	Cause   []interface{} `json:"cause"`
+}
+
+// PaymentMethods Access to Payment Methods
+func (c *Client) PaymentMethods(ctx context.Context) (PaymentMethods, error) {
+
+	url := fmt.Sprintf("%spayment_methods", c.BaseURL)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req = req.WithContext(ctx)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.token))
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
+		var errRes ErrorResponse
+		if err = json.NewDecoder(res.Body).Decode(&errRes); err == nil {
+			return nil, fmt.Errorf("Status code: %d - Error: %s - Message: %s", errRes.Status, errRes.Error, errRes.Message)
+		}
+
+		return nil, fmt.Errorf("unknown error, status code: %d", res.StatusCode)
+	}
+
+	var paymentMethods PaymentMethods
+	if err = json.NewDecoder(res.Body).Decode(&paymentMethods); err == nil {
+		return nil, errors.New("Can't parse response")
+
+	}
+
+	return paymentMethods, nil
+}
